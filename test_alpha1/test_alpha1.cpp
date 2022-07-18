74a7687bb8011b7e52a92f9fa33884b3049a301d
MiZhenxing <mizhenxing.henan@gmail.com>
2022-07-18 21:12:22 +0800

    update

--- test_alpha1/test_alpha1.cpp
+++ test_alpha1/test_alpha1.cpp
@@ -13,30 +13,13 @@ typedef pcl::PointCloud<PointT> PointCloudT;
 int main(int argc, char** argv)
 {
   PointCloudT::Ptr cloud(new PointCloudT);
-
-  for (double pointX = 0; pointX < 0.255; pointX += 0.05)
-  {
-    for (double pointY = 0; pointY < 0.255; pointY += 0.05)
-    {
-      PointT p;
-      p.x = pointX;
-      p.y = pointY;
-      p.z = 0;
-
-      p.r = 255;
-      p.g = 255;
-      p.b = pointX * 1000;
-
-      p.a = 255 - (pointX * 1000); // Set transparency
-      cloud->push_back(p);
-    }
-  }
+  pcl::io::loadPLYFile (argv[1], *cloud);
 
   pcl::visualization::PCLVisualizer viewer;
   viewer.setBackgroundColor (0.356, 0.356, 0.709);
   viewer.addPointCloud(cloud);
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 20);
-  pcl::io::savePLYFileASCII ("test_pcd.ply", *cloud);
+  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1);
+  // pcl::io::savePLYFileASCII ("test_pcd.ply", *cloud);
   // while (!viewer.wasStopped())
   viewer.spin();
 
